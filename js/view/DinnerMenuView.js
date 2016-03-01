6d81e428e25979638e920c84e283879a70283788
CTCbaxia <ctcbaxia@gmail.com>
2016-03-01 01:06:41 +0100

    Merge branch 'master' into Lab3_ctc
    # Conflicts:
    #	index.html
    #	js/view/DinnerMenuView.js

--- js/view/DinnerMenuView.js
+++ js/view/DinnerMenuView.js
@@ -1,8 +1,10 @@
 var DinnerMenuView = function(container,model){
 	model.attach(this);
-	this.dinnerMenu = container.find("#dinnerMenu");
+	this.dinnerMenu = container.find("#dinnerMenu #tableBody");
+	this.price = container.find("#totalPrice")
 	var menu = model.getFullMenu();
 	var menuList = "";
+
 	for (var i = 0; i < menu.length; i++) {
 	    //console.log(menu[i]);
 	    var id = menu[i].id;
@@ -10,26 +12,22 @@ var DinnerMenuView = function(container,model){
 	    var price = model.getTotalDishPrice(id);
 	    var totalPrice = model.getTotalMenuPrice();
 	    
-        menuList += "<table class=\"table\">" + 
-					"<tr>" +
+        menuList += "<tr>" +
 						"<td>"+ name +"</td>" +
 						"<td style=\"text-align:right;\">" + price + "</td>" +
-						"<td>"+ "<button type=\"button\" class=\"removeDish\" id="+ menu[i].id +">" + "delete" + "</button>" +"</td>" +
-					"</tr>" +
-				"</table>" ;
-	};
-        menuList += "<table class=\"table\">" + 
-					"<tr>" +
-						"<td>Total Price is </td>" +
-						"<td style=\"text-align:right;\">" + totalPrice + " SEK</td>" +
-					"</tr>" +
-				"</table>";
+						"<td>"+ "<button type=\"button\" class=\"removeDish\" id="+ menu[i].id +">" + "<span class = \"glyphicon glyphicon-remove\"></span></button>" +"</td>" +//每列添加了button
+					"</tr>" ;
 
+	};
+        
+    this.price.html(totalPrice);
 	this.dinnerMenu.html(menuList);
 
 	this.update = function(args){
+
 		if (args == "addMenu" || args =="people"|| args == "removeDish"||args == "backToMenu") {
 			this.dinnerMenu = container.find("#dinnerMenu");
+
 			var menu = model.getFullMenu();
 			var menuList = "";
 			for (var i = 0; i < menu.length; i++) {
@@ -38,35 +36,20 @@ var DinnerMenuView = function(container,model){
 	    	 var price = model.getTotalDishPrice(id);
 	    	 var totalPrice = model.getTotalMenuPrice();
 	    
-        menuList += "<table class=\"table\">" + 
-					"<tr>" +
+        menuList += "<tr>" +
 						"<td>"+ name +"</td>" +
 						"<td style=\"text-align:right;\">" + price + "</td>" +
-						"<td>"+ "<button type=\"button\"  class=\"removeDish\" id="+ menu[i].id +" >" + "delete" + "</button>" +"</td>" +
-					"</tr>" +
-				"</table>" ;
+						"<td>"+ "<button type=\"button\" class=\"removeDish\" id="+ menu[i].id +">" + "<span class = \"glyphicon glyphicon-remove\"></span>" + "</button>" +"</td>" +//每列添加了button
+					"</tr>" ;
 			};
 
 
-        menuList += "<table class=\"table\">" + 
-					"<tr>" +
-						"<td>Total Price is </td>" +
-						"<td style=\"text-align:right;\">" + totalPrice + " SEK</td>" +
-					"</tr>" +
-				"</table>";
+        this.price.html(totalPrice);
 
-	
-		 //    $(".removeDish").on("click",function(){
-		 //    var id = $(this).attr('id');
-		 //    model.removeDishFromMenu(id);
-		 //    model.notify("removeDish");
-		 //    });
-			
 		this.dinnerMenu.html(menuList);
 		$(".removeDish").click(removeDishFunction);
 
-		}
-		else if (args == "addPending") {
+		}else if (args == "addPending") {
 			this.dinnerMenu = container.find("#dinnerMenu");
 			var menu = model.getFullPendingMenu();
 			var menuList = "";
@@ -76,30 +59,23 @@ var DinnerMenuView = function(container,model){
 	    	 var price = model.getTotalDishPrice(id);
 	    	 
 	    
-	         menuList += "<table class=\"table\">" + 
-						"<tr>" +
-							"<td>"+ name +"</td>" +
-							"<td style=\"text-align:right;\">" + price + "</td>" +
-							"<td>"+ "<button type=\"button\"  class=\"removeDish\" id="+ menu[i].id +" >" + "delete" + "</button>" +"</td>" +//每列添加了button
-						"</tr>" +
-					"</table>" ;
+       		 menuList += "<tr>" +
+						"<td>"+ name +"</td>" +
+						"<td style=\"text-align:right;\">" + price + "</td>" +
+						"<td>"+ "<button type=\"button\" class=\"removeDish\" id="+ menu[i].id +">" + "<span class = \"glyphicon glyphicon-remove\"></span>" + "</button>" +"</td>" +//每列添加了button
+					"</tr>" ;
 			};
 			var index = menu.length-1;
 			var pendingID = menu[index].id;
-	        menuList += "<table class=\"table\">" + 
-						"<tr>" +
-							"<td>"+ "pending" +"</td>" +
-							"<td style=\"text-align:right;\">" + model.getTotalDishPrice(pendingID) + "</td>" +
-							// "<td>"+ "<button type=\"button\"  class=\"removeDish\" id="+ menu[index].id +" >" + "delete" + "</button>" +"</td>" +//每列添加了button
-						"</tr>" +
-					"</table>" ;
+			menuList += "<tr>" +
+						"<td>"+ "pending" +"</td>" +
+						"<td style=\"text-align:right;\">" + model.getTotalDishPrice(pendingID) + "</td>" +
+						// "<td>"+ "<button type=\"button\" class=\"removeDish\" id="+ menu[i].id +">" + "<span class = \"glyphicon glyphicon-remove\"></span>" + "</button>" +"</td>" +//每列添加了button
+					"</tr>" ;
+			
+
 			var totalPrice = model.getTotalMenuPrice();
-	        menuList += "<table class=\"table\">" + 
-						"<tr>" +
-							"<td>Total Price is </td>" +
-							"<td style=\"text-align:right;\">" + totalPrice + " SEK</td>" +
-						"</tr>" +
-					"</table>";
+        	this.price.html(totalPrice);
 
 	
 		 //    $(".removeDish").on("click",function(){
@@ -113,7 +89,8 @@ var DinnerMenuView = function(container,model){
 
 		};
 
-	}
 
+
+	}
 }
 
